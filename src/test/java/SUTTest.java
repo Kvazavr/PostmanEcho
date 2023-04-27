830858a72f872287c405aa8ecb5ecc3407b71b12
Elena Nekrasova <kvazavr2108@gmail.com>
2023-04-27 20:38:08 +0400

    Initial commit

---
+++ src/test/java/SUTTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class SUTTest {
+    @Test
+    void shouldReturnAnswer() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Ха-ха-ха-ха-ха")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Ха-ха-ха-ха-ха"))
+        ;
+    }
+}
